141be3a97fcdd1eec9dac57bfeadb9458df52610
webeweb <support@webeweb.fr>
2021-06-28 12:32:55 +0200

    Add date/time updated at trait

---
+++ tests/Fixtures/DateTimes/TestDateTimeUpdatedAtTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the traits-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Fixtures\DateTimes;
+
+use WBW\Library\Traits\DateTimes\DateTimeUpdatedAtTrait;
+
+/**
+ * Test date/time updated at trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\Fixtures\DateTimes
+ */
+class TestDateTimeUpdatedAtTrait {
+
+    use DateTimeUpdatedAtTrait;
+}
