f88d95362e659b07ca144dfbe99b3e57c2e99609
webeweb <support@webeweb.fr>
2021-06-28 18:04:03 +0200

    Add composite address trait

---
+++ tests/Composite/CompositeAddressTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the traits-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Traits\Tests\Composite;
+
+use WBW\Library\Traits\Tests\AbstractTestCase;
+use WBW\Library\Traits\Tests\Fixtures\Composite\TestCompositeAddressTrait;
+
+/**
+ * Composite address trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Traits\Tests\Composite
+ */
+class CompositeAddressTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setAddresseeDescription() method.
+     *
+     * @return void
+     */
+    public function testSetAddresseeDescription(): void {
+
+        $obj = new TestCompositeAddressTrait();
+
+        $obj->setAddresseeDescription("addresseeDescription");
+        $this->assertEquals("addresseeDescription", $obj->getAddresseeDescription());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct() {
+
+        $obj = new TestCompositeAddressTrait();
+
+        $this->assertNull($obj->getAddressee());
+        $this->assertNull($obj->getCountry());
+        $this->assertNull($obj->getHouseNumber());
+        $this->assertNull($obj->getLocation());
+        $this->assertNull($obj->getPostalCode());
+        $this->assertNull($obj->getStreetName());
+    }
+}
